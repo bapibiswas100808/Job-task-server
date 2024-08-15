a0a86fc565430d677f6479f504d9c92e458e423a
bapibiswas100808 <star.bappy@gmail.com>
2024-08-15 19:59:29 +0600

    server started

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5e8b5ac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const ProductCollections = client.db("JobTaskDB").collection("allProducts");
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // Get All Product
+    app.get("/allProducts", async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+
+      const skip = (page - 1) * limit;
+      const allProducts = await ProductCollections.find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      const totalProducts = await ProductCollections.countDocuments();
+
+      res.send({
+        data: allProducts,
+        totalPages: Math.ceil(totalProducts / limit),
+        currentPage: page,
+      });
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("project is running");
+});
+
+app.listen(port, () => {
+  console.log(`project is running at ${port}`);
+});
